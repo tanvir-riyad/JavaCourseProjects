013d4b1adddd6f0492f0d368748bc4bc523ba2bf
Tanvir Riyad <tanvirriyad12@gmail.com>
2022-03-06 11:56:01 +0100

    Create Exercsie01_03.java

---
+++ Exercsie01_03.java
@@ -0,0 +1,12 @@
+public class Exercise_01_03 {
+
+	public static void main(final String[] args) {
+		System.out.println("The result is: "+ add(5,3.5));
+	}
+
+	// Implement the function as described in the exercise sheet
+	public static double add(double a, double b) {
+		return a+b;
+	}
+
+}
