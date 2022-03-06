6fe01fce62c6faf69dc828fa0683e0faedef7950
Tanvir Riyad <tanvirriyad12@gmail.com>
2022-03-06 11:57:58 +0100

    Create Exercsie01_05.java

---
+++ Exercsie01_05.java
@@ -0,0 +1,9 @@
+public class Exercise_01_05 {
+
+	public static void main(final String[] args) {
+		System.out.println("Exercise 01 05");
+	}
+
+	// Task: create your own function signature to solve the task
+
+}
