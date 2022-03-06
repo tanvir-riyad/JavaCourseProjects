230dbb16c294e9b29d923422cefba7e2672bbf02
Tanvir Riyad <tanvirriyad12@gmail.com>
2022-03-06 11:57:09 +0100

    Create Exercsie01_04.java

---
+++ Exercsie01_04.java
@@ -0,0 +1,11 @@
+public class Exercise_01_04 {
+	
+	public static double calculatePerimeter(double radius) {
+		return 2*Math.PI*radius;
+	}
+
+	public static void main(final String[] args) {
+		System.out.println("System.out.println("the permimeter of the circle is "+ calculatePerimeter(5))");
+	}
+	
+}
