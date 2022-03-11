3a857bf1b14921d623a1e2c5f387f33d05312e65
Tanvir Riyad <tanvirriyad12@gmail.com>
2022-03-11 10:51:02 +0100

    Create BucketOfObjects.java

---
+++ Exercise08/BucketOfObjects.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public  class BucketOfObjects {
+	
+	private List<GeometricObject> items=new ArrayList<GeometricObject>();
+	
+	
+	
+	public BucketOfObjects() {
+		
+		
+	}
+	
+	public void addObject(GeometricObject go) {
+		
+		items.add(go);
+		}
+		
+		
+	
+	
+	public double getTotalArea() {
+		
+		double a=0;
+		
+		for(int i=0;i<items.size();i++) {
+			
+			a=a+items.get(i).getArea();	
+			
+		}
+		
+		return a;
+	}
+	
+	public double getTotalPerimeter() {
+		
+		double a=0;
+		
+		for(int i=0;i<items.size();i++) {
+			
+			a=a+items.get(i).getPerimeter();	
+			
+		}
+		
+		return a;
+	}
+
+	public String toString() {
+		
+		
+		
+		ArrayList<String> str = new ArrayList<String>();
+	    for (int i = 0; i < items.size(); i++) {
+	        str.add(items.get(i).getClass().getName());
+	        
+	    }
+	    
+	    String a=str.toString();
+	    
+	    a = a.replace("[", "")
+	            .replace("]", "");
+	            //.replace(" ", "");    
+	    //System.out.println(a);
+		
+		
+		return a;
+	
+	
+}
+	
+	public void removeObject(GeometricObject go) {
+		
+		items.remove(go);
+		
+	}
+}
